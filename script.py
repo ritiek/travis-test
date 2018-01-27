3421c5a57fc78770dc5673742c343a4017c8e9fe
ritiek <ritiekmalhotra123@gmail.com>
2018-01-27 20:28:10 +0530

    Test regression

--- script.py
+++ script.py
@@ -42,7 +42,8 @@ mid_date = middle_date(start_date, end_date)
 toolchain = '{0}-{1}'.format(toolchain_type, mid_date)
 
 urllib.request.urlretrieve('http://sh.rustup.rs', 'rustup.sh')
-subprocess.run(['bash', 'rustup.sh', '--default-toolchain', toolchain])
+subprocess.run(['bash', 'rustup.sh', '--default-toolchain', toolchain
+                '<', '1'])
 
 while True:
     if output_has_keywords(cmd, keywords):
